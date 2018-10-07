4923c95476573ceda347cd0d1d8bfceba0cdd1d4
Ari Marin <ari.aarno.kristian.marin@gmail.com>
2018-10-07 21:20:55 +0300

    Added PetTypeService. Closes #29.

---
+++ pet-clinic-data/src/main/java/udemy/spring/course/petclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package udemy.spring.course.petclinic.services;
+
+import udemy.spring.course.petclinic.model.PetType;
+
+/**
+ * Created by Ari on 07.10.2018
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
