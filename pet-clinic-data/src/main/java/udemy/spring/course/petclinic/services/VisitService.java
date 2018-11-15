f8204b4a1bf4da1c1143698197dc3cf30b412c9b
Ari Marin <ari.aarno.kristian.marin@gmail.com>
2018-11-15 22:05:23 +0200

    Implemented Map impl for Visit Service. Closes #46.

---
+++ pet-clinic-data/src/main/java/udemy/spring/course/petclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package udemy.spring.course.petclinic.services;
+
+import udemy.spring.course.petclinic.model.Visit;
+
+/**
+ * Created by Ari on 15.11.2018
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
